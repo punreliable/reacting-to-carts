4bde60178689ae3339ca49899f9913c9f9fb2a6b
Brian <briansmith@rocketcentral.com>
2023-02-07 17:24:56 -0500

    Cart is toggleable

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-configureStore({
-    
+import uiSlice from './ui-slice';
+
+const store = configureStore({
+    reducer: {ui: uiSlice.reducer}
 });
+
+export default store;
