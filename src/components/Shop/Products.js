9d99c8eb2137d1e19575d6239a1f7580bf968f4f
Brian <briansmith@rocketcentral.com>
2023-02-07 17:55:39 -0500

    Using objects to dispatch new item to cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 3,
+    title: "My First Book",
+    description: "The First Book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 3,
+    title: "My Second Book",
+    description: "The Second Book I ever wrote",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map( (product) => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
+        ))}
       </ul>
     </section>
   );
