0e7245818afa6715e2161a77ae9f0b92add368d5
Smith <BSmith9@cg9p9d0gyd.mi.corp.rockfin.com>
2023-02-08 17:51:34 -0500

    Fixed Notification component and '-' cart item component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,79 @@
-import { response } from "express";
-import { Fragment, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
-import Notification from "./components/UI/Notification";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
-
   const dispatch = useDispatch();
-  const showCart = useSelector( state => state.ui.cartIsVisible );
-  const cart = useSelector( (state) => state.cart );
-  const notification = useState( (state) => state.ui.notification );
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
-  useEffect( () => {
+  useEffect(() => {
     const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending..',
-        message: 'Sending cart data!',
-      })
-
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
       );
-    
-    const response = fetch('https://reacting-to-hunger-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+      const response = await fetch(
+        'https://reacting-to-hunger-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
     });
-
-    if(!response.ok) {
-      
-      throw new Error('Sending cart data failed!');
-
-    }
-    
-    const responseData = await response.json();
-    
-    dispatch(
-      uiActions.showNotification({
-      status: 'success',
-      title: 'Success!',
-      message: 'Sent Cart data successfully!',
-    }),
-    dispatch(
-      uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!',
-      })
-    );
-  
-    }
   }, [cart, dispatch]);
 
   return (
     <Fragment>
-      {notification && ( 
-        <Notification 
+      {notification && (
+        <Notification
           status={notification.status}
           title={notification.title}
           message={notification.message}
         />
       )}
-    <Layout>
-      {showCart && <Cart /> }
-      <Products />
-    </Layout>
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
     </Fragment>
   );
 }
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,21 +4,22 @@ import classes from './CartItem.module.css';
 import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-
   const dispatch = useDispatch();
 
   const { title, quantity, total, price, id } = props.item;
 
   const removeItemHandler = () => {
-    dispatch(cartActions.removeItemFromCart({}));
+    dispatch(cartActions.removeItemFromCart(id));
   };
 
   const addItemHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id,
-      title,
-      price,
-    }));
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
